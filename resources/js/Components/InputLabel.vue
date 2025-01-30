b23a20d233648f5b92ad557d432f7573acfdfbc6
ruan <ruan.preto@universo.univates.br>
2025-01-29 23:50:29 -0300

    feat: adicionado estilo para campo requerido

--- resources/js/Components/InputLabel.vue
+++ resources/js/Components/InputLabel.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 defineProps<{
     value?: string;
+    inputRequired?: boolean;
 }>();
 </script>
 
 <template>
     <label class="block text-sm font-medium text-gray-700">
-        <span v-if="value">{{ value }}</span>
+        <span v-if="value">
+          {{ value }}
+          <span v-if="inputRequired" class="text-red-500">*</span>
+        </span>
         <span v-else><slot /></span>
     </label>
 </template>
