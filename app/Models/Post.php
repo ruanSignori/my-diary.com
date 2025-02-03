72be93de4c076b9e778a9b5dd069a852b2574c07
Ruan <ruan@solis.com.br>
2025-02-03 12:30:06 -0300

    fix: ajustado nome da migration e da model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,11 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Posts extends Model
+class Post extends Model
 {
   use HasFactory;
-
-  protected $table = 'posts';
 
   protected $fillable = [
     'title',
