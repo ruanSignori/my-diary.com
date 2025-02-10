4da7d97c5e7c466f4b60168672c6971d4cbf48c0
ruan <ruan.preto@universo.univates.br>
2025-02-10 01:22:24 -0300

    feat: adicionado relacionamento post-user

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,11 +16,19 @@ class Post extends Model
     'author_id'
   ];
 
+  public function user()
+  {
+      return $this->belongsTo(User::class, 'author_id');
+  }
+
   public static function findByOwnerAndSlug($ownerId, string $slug): Post
   {
-      return self::where('author_id', $ownerId)
-                 ->where('slug', $slug)
-                 ->first();
+    $data = self::select('posts.*', 'users.name as author')
+                ->join('users', 'posts.author_id', '=', 'users.id')
+                ->where('author_id', $ownerId)
+                ->where('slug', $slug)
+                ->first();
+    return $data;
   }
 
 }
