2af31f0f71dd4df21db100525855d5089a8103d1
Ruan Signori <ruan@solis.com.br>
2025-04-28 13:21:32 -0300

    WIP: ajustes no componente de `MultiSelect`

--- resources/js/Components/Inputs/MultiSelect.vue
+++ resources/js/Components/Inputs/MultiSelect.vue
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/vue';
 import Checkbox from '@/Components/Inputs/Checkbox.vue';
 
 interface Option {
-  value: string | number;
+  value: string | number | null;
   label: string;
   color?: string;
 }
@@ -27,7 +27,7 @@ const colorPalette = {
 };
 
 const selectedItems = computed(() => {
-  return props.options.filter(option => props.modelValue.includes(option.value));
+  return props.options.filter(option => props.modelValue.includes(option.value as string));
 });
 
 const filteredOptions = computed(() => {
@@ -60,12 +60,20 @@ const closeDropdown = () => {
 };
 
 const isSelected = (option: Option): boolean => {
-  return props.modelValue.includes(option.value);
+  return props.modelValue.includes(option.value as string);
 };
 
 const addItem = (option: Option) => {
   const newValue = [...props.modelValue];
-  newValue.push(option.value);
+
+  if (isSelected(option)) {
+    const index = newValue.indexOf(option.value as string);
+    if (index !== -1) {
+      newValue.splice(index, 1);
+    }
+  } else {
+    newValue.push(option.value as string);
+  }
 
   emit('update:modelValue', newValue);
 };
@@ -94,10 +102,8 @@ const vClickOutside = {
 <template>
   <div class="relative" v-click-outside="closeDropdown">
     <!-- Header do select -->
-    <div
-      @click="toggleDropdown"
-      class="w-full mt-1 flex flex-wrap min-h-10 items-center p-2 bg-white border border-gray-300 shadow-sm rounded-md cursor-pointer focus:outline-none focus:border-[var(--color-primary)] focus:ring-[var(--color-primary)]"
-    >
+    <div @click="toggleDropdown"
+      class="w-full mt-1 flex flex-wrap min-h-10 items-center p-2 bg-white border border-gray-300 shadow-sm rounded-md cursor-pointer focus:outline-none focus:border-[var(--color-primary)] focus:ring-[var(--color-primary)]">
       <!-- Ícone "Lista" à esquerda -->
       <div class="mr-2 text-gray-400" v-if="!selectedItems.length">
         <Icon icon="ri:list-unordered" width="18" height="18" />
@@ -108,69 +114,48 @@ const vClickOutside = {
         {{ placeholder || 'Selecione uma ou mais opções' }}
       </div>
       <div v-else class="flex flex-wrap gap-1">
-        <div
-          v-for="item in selectedItems"
-          :key="item.value"
+        <div v-for="item in selectedItems" :key="item.value as string"
           class="flex items-center rounded-md px-2 py-1 text-xs"
-          :style="{ backgroundColor: colorPalette.bg, color: colorPalette.text }"
-        >
+          :style="{ backgroundColor: colorPalette.bg, color: colorPalette.text }">
           <span>{{ item.label }}</span>
-          <button
-            @click.stop="removeItem(item)"
-            class="ml-1 hover:opacity-75 focus:outline-none"
-          >
-          <Icon icon="ri:close-line" width="16" height="16" />
+          <button @click.stop="removeItem(item)" class="ml-1 hover:opacity-75 focus:outline-none">
+            <Icon icon="ri:close-line" width="16" height="16" />
           </button>
         </div>
       </div>
     </div>
 
     <!-- Dropdown menu -->
-    <div
-      v-if="isOpen"
-      class="absolute z-10 w-full mt-1 bg-white border border-gray-200 rounded-md shadow-lg max-h-60 overflow-auto"
-    >
+    <div v-if="isOpen"
+      class="absolute z-10 w-full mt-1 bg-white border border-gray-200 rounded-md shadow-lg max-h-60 overflow-auto">
       <div class="sticky top-0 bg-white border-b border-gray-100">
-        <input
-          type="text"
-          v-model="searchQuery"
-          placeholder="Selecione uma opção ou crie uma"
-          class="w-full p-2 text-sm focus:outline-none border-none"
-          @click.stop
-        >
+        <input type="text" v-model="searchQuery" placeholder="Selecione uma opção ou crie uma"
+          class="w-full p-2 text-sm focus:outline-none border-none" @click.stop>
       </div>
 
       <!-- Se não encontrou nenhum opção, permite criar uma-->
-      <div v-if="filteredOptions.length === 0" class="p-2 text-gray-500 text-start text-sm flex items-center gap-2">
+      <div v-if="filteredOptions.length === 0" class="p-2 text-gray-500 text-start text-sm flex items-center gap-2"
+        @click.stop="addItem({
+          value: null,
+          label: searchQuery as string,
+        })">
         Criar
-          <div
-            class="rounded-md px-2 py-1 text-xs"
-            :style="{ backgroundColor: colorPalette.bg, color: colorPalette.text }"
-          >
-          {{searchQuery}}
+        <div class="rounded-md px-2 py-1 text-xs"
+          :style="{ backgroundColor: colorPalette.bg, color: colorPalette.text }">
+          {{ searchQuery }}
         </div>
       </div>
 
       <!-- Exibe as opções -->
       <div v-else>
-        <div
-          v-for="option in filteredOptions"
-          :key="option.value"
-          @click.stop="addItem(option)"
-          class="p-2 hover:bg-gray-50 cursor-pointer flex items-center gap-2 text-sm group"
-        >
-          <Checkbox
-            :checked="isSelected(option)"
-            name="selected_category"
-            @click.stop="addItem(option)"
-            aria-readonly="true"
-          />
+        <div v-for="option in filteredOptions" :key="option.value as string" @click.stop="addItem(option)"
+          class="p-2 hover:bg-gray-50 cursor-pointer flex items-center gap-2 text-sm group">
+          <Checkbox :checked="isSelected(option)" name="selected_category" @click.stop="addItem(option)"
+            aria-readonly="true" />
 
           <!-- Tag colorida com o nome da opção -->
-          <div
-            class="rounded-md px-2 py-1 text-xs"
-            :style="{ backgroundColor: colorPalette.bg, color: colorPalette.text }"
-          >
+          <div class="rounded-md px-2 py-1 text-xs"
+            :style="{ backgroundColor: colorPalette.bg, color: colorPalette.text }">
             {{ option.label }}
           </div>
         </div>
