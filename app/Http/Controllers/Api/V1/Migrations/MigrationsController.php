db711d042075a75f9158feea7e8881e97016cb62
ruan <ruan.preto@universo.univates.br>
2025-05-02 10:28:57 -0300

    refactor: utilizado variavies de status http ao invés de numeros hardcoded

--- app/Http/Controllers/Api/V1/Migrations/MigrationsController.php
+++ app/Http/Controllers/Api/V1/Migrations/MigrationsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1\Migrations;
 use App\Http\Controllers\Controller;
 use App\Services\MigrationsService;
 use App\Services\ResponseService;
+use Illuminate\Http\Response;
 
 class MigrationsController extends Controller
 {
@@ -20,7 +21,7 @@ class MigrationsController extends Controller
     public function store()
     {
       $doneMigrations = MigrationsService::runPendingMigrations();
-      $statusCode = count($doneMigrations) > 0 ? 201 : 200;
+      $statusCode = count($doneMigrations) > 0 ? Response::HTTP_CREATED : RESPONSE::HTTP_OK;
 
        return ResponseService::success([
         'migrations' => $doneMigrations
